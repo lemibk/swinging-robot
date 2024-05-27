10f3d5f28950b4ec07785b593ae1f4082e44b335
lemibk <lemibidikasenbeta@gmail.com>
2024-05-27 21:05:39 +0300

    the robot

--- Fun/main.cpp
+++ Fun/main.cpp
@@ -35,15 +35,453 @@ glColor3f(0.7, 0.7, 0.7);
 	gluCylinder(tri, 1, 1, 3, 32, 32);
 	glPopMatrix();
 	//the top pole
+
 	glPushMatrix();
+	glRotatef(90, 10, 0, 0);
 	glColor3f(0.7, 0.7, 0.7);
 	glTranslatef(5, 0, -10);
 	glScalef(0.5, 5, 1);
 	glRotatef(90, 1, 0, 0);
-	glRotatef(90, 10, 0, 0);
+
 	GLUquadric* top = gluNewQuadric();
 	gluCylinder(top, 1, 1, 3, 32, 32);
 	glPopMatrix();
+
+	glPushMatrix();
+	//glRotatef(50, 0, 1, 0);
+	glScalef(1, 0.5, 1);
+	glTranslatef(0, -15, 0);
+	glPushMatrix();
+	glTranslatef(0, 8, 0);
+	glScalef(4, 2, 1);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0.7, 0.7, 0.7);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+	glEnd();
+	glPopMatrix();
+	//the eyes
+
+	glPushMatrix();
+
+	glTranslatef(-2, 8, 0);
+	glColor3f(1, 1, 1);
+	glScalef(1, 0.5, 1);
+	glutSolidSphere(2, 32, 32);
+	glPopMatrix();
+	glPushMatrix();
+
+	glTranslatef(2, 8, 0);
+	glColor3f(1, 1, 1);
+	glScalef(1, 0.5, 1);
+	glutSolidSphere(2, 32, 32);
+	glPopMatrix();
+
+	//the neck
+	glPushMatrix();
+
+
+	glTranslatef(0, 6, 0);
+	glScalef(1, 2, 1);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0.7, 0.7, 0.7);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+	glEnd();
+	glPopMatrix();
+	//the stomack
+	glPushMatrix();
+	glTranslatef(0, 2, 0);
+	glScalef(4, 2, 1);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+	glEnd();
+	glPopMatrix();
+	//the left leg
+	glPushMatrix();
+	glTranslatef(-2, -2, 0);
+	glScalef(1, 2, 1);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+	glEnd();
+	glPopMatrix();
+	//the right leg
+	glPushMatrix();
+	glTranslatef(2, -2, 0);
+	glScalef(1, 2, 1);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 0);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+	glEnd();
+	glPopMatrix();
+	//the shoes
+	glPushMatrix();
+	glTranslatef(2, -5, 0);
+	glScalef(1.3, 1, 1);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(1, 1, 1);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+	glEnd();
+	glPopMatrix();
+	//left shoe
+	glPushMatrix();
+	glTranslatef(-2, -5, 0);
+	glScalef(1.3, 1, 1);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(1, 1, 1);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+	glEnd();
+	glPopMatrix();
+	//the hands
+	glPushMatrix();
+	glTranslatef(-5, 2, 0);
+	glRotatef(300, 0, 0, 1);
+	glScalef(1, 2, 1);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0.7, 0.7, 0.7);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+	glEnd();
+	glPopMatrix();
+	//the right hand
+	glPushMatrix();
+	glTranslatef(5, 2, 0);
+	glRotatef(50, 0, 0, 1);
+	glScalef(1, 2, 1);
+
+	glColor3f(1, 0, 0);
+	glBegin(GL_QUADS);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, -1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(-1, 1, 1);
+
+
+	glColor3f(1, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(-1, -1, 1);
+
+	glColor3f(0, 1, 0);
+	glVertex3f(1, -1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0.7, 0.7, 0.7);
+	glVertex3f(-1, -1, 1);
+	glVertex3f(-1, 1, 1);
+	glVertex3f(1, 1, 1);
+	glVertex3f(1, -1, 1);
+
+	glColor3f(0, 0, 1);
+	glVertex3f(-1, -1, -1);
+	glVertex3f(-1, 1, -1);
+	glVertex3f(1, 1, -1);
+	glVertex3f(1, -1, -1);
+	glEnd();
+	glPopMatrix();
+	//the mouth
+	glPushMatrix();
+	glColor3f(1, 0, 1);
+	glTranslatef(0, 7, 0);
+	glScalef(1, 0.5, 1);
+	glutSolidCube(2);
+	glPopMatrix();
+	glPopMatrix();
+	glPushMatrix();
+	glColor3f(0, 1, 1);
+	glTranslatef(0, -2, 0);
+	glScalef(5, 1, 80.0f);
+
+	glPopMatrix();
+	
 
 	glFlush();
 
