3424527895b4dbea7735cff01e7a8ad18d0113fb
lemibk <lemibidikasenbeta@gmail.com>
2024-05-30 00:29:52 +0300

    done

--- Fun/main.cpp
+++ Fun/main.cpp
@@ -1,5 +1,8 @@
 #include<GL/glut.h>
 #include<math.h>
+float r = 1.0f, g = 0.0f, b = 0.0f;
+float tx = 0, ty = -15, tz = 0;
+float xr= 0.001,angle = -40.0f;
 void init() {
 	glClearColor(1, 1, 1, 1);
 	glMatrixMode(GL_PROJECTION);
@@ -12,7 +15,7 @@ void display() {
 	//the grass
 	glPushMatrix();
 	glTranslatef(0,-10,0);
-	glScalef(20, 10, 10);
+	glScalef(15, 10, 10);
 	glColor3f(0,1,0);
 	glutSolidCube(3);
 	glPopMatrix();
@@ -37,20 +40,20 @@ glColor3f(0.7, 0.7, 0.7);
 	//the top pole
 
 	glPushMatrix();
-	glRotatef(90, 10, 0, 0);
+	
 	glColor3f(0.7, 0.7, 0.7);
-	glTranslatef(5, 0, -10);
-	glScalef(0.5, 5, 1);
-	glRotatef(90, 1, 0, 0);
+	glTranslatef(-12, 0, 0);
+	glScalef(1, 1, 1);
+	glRotatef(90, 0, 1, 0);
 
 	GLUquadric* top = gluNewQuadric();
-	gluCylinder(top, 1, 1, 3, 32, 32);
+	gluCylinder(top, 1, 1, 23, 32, 32);
 	glPopMatrix();
 
 	glPushMatrix();
-	//glRotatef(50, 0, 1, 0);
+	glRotatef(angle, 1, 0, 0);
 	glScalef(1, 0.5, 1);
-	glTranslatef(0, -15, 0);
+	glTranslatef(tx, ty, tz);
 	glPushMatrix();
 	glTranslatef(0, 8, 0);
 	glScalef(4, 2, 1);
@@ -62,20 +65,20 @@ glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(1, -1, 1);
 	glVertex3f(-1, -1, 1);
 
-	glColor3f(0, 1, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
 	glVertex3f(-1, 1, 1);
 
 
-	glColor3f(1, 0, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(-1, 1, 1);
 	glVertex3f(-1, -1, 1);
 
-	glColor3f(0, 1, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(1, -1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
@@ -86,8 +89,7 @@ glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, 1, 1);
 	glVertex3f(1, 1, 1);
 	glVertex3f(1, -1, 1);
-
-	glColor3f(0, 0, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
@@ -100,14 +102,14 @@ glColor3f(0.7, 0.7, 0.7);
 
 	glTranslatef(-2, 8, 0);
 	glColor3f(1, 1, 1);
-	glScalef(1, 0.5, 1);
+	glScalef(0.5, 0.5, 1);
 	glutSolidSphere(2, 32, 32);
 	glPopMatrix();
 	glPushMatrix();
 
 	glTranslatef(2, 8, 0);
 	glColor3f(1, 1, 1);
-	glScalef(1, 0.5, 1);
+	glScalef(0.5, 0.5, 1);
 	glutSolidSphere(2, 32, 32);
 	glPopMatrix();
 
@@ -118,27 +120,26 @@ glColor3f(0.7, 0.7, 0.7);
 	glTranslatef(0, 6, 0);
 	glScalef(1, 2, 1);
 
-	glColor3f(1, 0, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glBegin(GL_QUADS);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(1, -1, -1);
 	glVertex3f(1, -1, 1);
 	glVertex3f(-1, -1, 1);
 
-	glColor3f(0, 1, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
 	glVertex3f(-1, 1, 1);
 
 
-	glColor3f(1, 0, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(-1, 1, 1);
 	glVertex3f(-1, -1, 1);
-
-	glColor3f(0, 1, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(1, -1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
@@ -150,7 +151,7 @@ glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(1, 1, 1);
 	glVertex3f(1, -1, 1);
 
-	glColor3f(0, 0, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
@@ -162,39 +163,39 @@ glColor3f(0.7, 0.7, 0.7);
 	glTranslatef(0, 2, 0);
 	glScalef(4, 2, 1);
 
-	glColor3f(1, 0, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glBegin(GL_QUADS);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(1, -1, -1);
 	glVertex3f(1, -1, 1);
 	glVertex3f(-1, -1, 1);
 
-	glColor3f(0, 1, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
 	glVertex3f(-1, 1, 1);
 
 
-	glColor3f(1, 0, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(-1, 1, 1);
 	glVertex3f(-1, -1, 1);
 
-	glColor3f(0, 1, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(1, -1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
 	glVertex3f(1, -1, 1);
 
-	glColor3f(0, 0, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, 1);
 	glVertex3f(-1, 1, 1);
 	glVertex3f(1, 1, 1);
 	glVertex3f(1, -1, 1);
 
-	glColor3f(0, 0, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
@@ -379,31 +380,30 @@ glColor3f(0.7, 0.7, 0.7);
 	glPopMatrix();
 	//the hands
 	glPushMatrix();
-	glTranslatef(-5, 2, 0);
-	glRotatef(300, 0, 0, 1);
-	glScalef(1, 2, 1);
+	glTranslatef(-6, 7, 0);
+	glRotatef(-340, 0, 0, 1);
+	glScalef(1, 4, 1);
 
-	glColor3f(1, 0, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glBegin(GL_QUADS);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(1, -1, -1);
 	glVertex3f(1, -1, 1);
 	glVertex3f(-1, -1, 1);
 
-	glColor3f(0, 1, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
 	glVertex3f(-1, 1, 1);
 
-
-	glColor3f(1, 0, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(-1, 1, 1);
 	glVertex3f(-1, -1, 1);
 
-	glColor3f(0, 1, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(1, -1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
@@ -415,7 +415,7 @@ glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(1, 1, 1);
 	glVertex3f(1, -1, 1);
 
-	glColor3f(0, 0, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
@@ -424,31 +424,30 @@ glColor3f(0.7, 0.7, 0.7);
 	glPopMatrix();
 	//the right hand
 	glPushMatrix();
-	glTranslatef(5, 2, 0);
-	glRotatef(50, 0, 0, 1);
-	glScalef(1, 2, 1);
+	glTranslatef(6, 7, 0);
+	glRotatef(150, 0, 0, 1);
+	glScalef(1, 4, 1);
 
-	glColor3f(1, 0, 0);
+	glColor3f(0.7, 0.7, 0.7);
 	glBegin(GL_QUADS);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(1, -1, -1);
 	glVertex3f(1, -1, 1);
 	glVertex3f(-1, -1, 1);
-
-	glColor3f(0, 1, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
 	glVertex3f(-1, 1, 1);
 
 
-	glColor3f(1, 0, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(-1, 1, 1);
 	glVertex3f(-1, -1, 1);
 
-	glColor3f(0, 1, 0);
+	glColor3f(0.7, 0.7, 0.7);;
 	glVertex3f(1, -1, -1);
 	glVertex3f(1, 1, -1);
 	glVertex3f(1, 1, 1);
@@ -460,7 +459,7 @@ glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(1, 1, 1);
 	glVertex3f(1, -1, 1);
 
-	glColor3f(0, 0, 1);
+	glColor3f(0.7, 0.7, 0.7);
 	glVertex3f(-1, -1, -1);
 	glVertex3f(-1, 1, -1);
 	glVertex3f(1, 1, -1);
@@ -489,6 +488,45 @@ glColor3f(0.7, 0.7, 0.7);
 
 	
 }
+void keyPress(unsigned char key, int x, int y) {
+	switch (key)
+	{
+	case 'l':
+		ty = ty + 2;
+		break;
+	case 'f':
+		xr = xr + 0.005;
+		break;
+	case 's':
+		xr = xr - 0.005;
+		break;
+	case 'p':
+		xr = 0;
+		angle = 0;
+		break;
+	case 'q':
+		xr =  0.05;
+		break;
+	
+	}
+	glutPostRedisplay();
+}
+void animate() {
+	
+		
+	
+	 if (angle < 40.0f) {
+		angle = angle + xr;
+
+	 }
+	 
+	 else {
+		angle = -40.0f;
+	}
+	
+
+	glutPostRedisplay();
+}
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
@@ -497,7 +535,8 @@ int main(int argc, char** argv) {
 	glutInitWindowSize(600, 600);
 	glutCreateWindow("cube");
 	init();
+	glutKeyboardFunc(keyPress);
 	glutDisplayFunc(display);
-
+    glutIdleFunc(animate);
 	glutMainLoop();
 }
